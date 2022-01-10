ec6ddea568e4c2fc743b74f1aea63acf5ef2edf8
NataliItaly <87366835+NataliItaly@users.noreply.github.com>
2022-01-10 01:06:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const BURGER = document.querySelector('.burger');
+const HEADERMENU = document.querySelector('.header-menu');
+
+BURGER.addEventListener('click', function() {
+    BURGER.classList.toggle('burger-active');
+    HEADERMENU.classList.toggle('menu-open');
+});
+HEADERMENU.addEventListener('click', function() {
+    BURGER.classList.toggle('burger-active');
+    HEADERMENU.classList.toggle('menu-open');
+});
